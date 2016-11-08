c3874624f0f42be712325d85b8332ac472125edf
haruharuharuby <cocktail.house@gmail.com>
2016-11-09 02:16:19 +0900

    recognize event

--- avs.py
+++ avs.py
@@ -14,26 +14,30 @@ class Avs:
   def __init__(self):
     self.downstram_stop_signal = threading.Event()
     self.connection = HTTP20Connection(host=self.ENDPOINT, secure=True, force_proto='h2', enable_push=True)
+    print("[STATE:INIT] Connection created.")
     self.establish_downstream()
     self.synchronize_to_avs()
 
   def establish_downstream(self):
     header = {"Authorization": "Bearer %s" % (self.gettoken())}
     self.downstream_id = self.connection.request(method="GET", url=self.__interface("directives"), headers=header)
-    downstream_response = self.connection.get_response(downstream_id)
+    downstream_response = self.connection.get_response(self.downstream_id)
     if downstream_response.status != 200:
       raise NameError("Bad downstream response %s" % (downstream_response.status))
-    self.downstream_boundary = get_downstream_boundary(response)
+    self.downstream_boundary = self.get_downstream_boundary(downstream_response)
+    print("[STATE:INIT] downstream established. bounday=%s" % (self.downstream_boundary))
     downstream_polling = threading.Thread(target=self.downstram_polling_thread)
     downstream_polling.start()
+    print("[STATE:INIT] downstream polling start")
 
   def synchronize_to_avs(self):
     boundary_name = 'synchronization-term'
     header = self.__header(boundary_name)
-    stream_id = conn.request(method="POST", url=self.__interface("events"), headers=header, body=self.__synchronize_message(boundary_name))
-    res = conn.get_response(stream_id)
+    stream_id = self.connection.request(method="POST", url=self.__interface("events"), headers=header, body=self.__synchronize_message(boundary_name))
+    res = self.connection.get_response(stream_id)
     if res.status != 204:
       raise NameError("Bad synchronize response %s" % (res.status))
+    print("[STATE:INIT] synchronize to AVS succeeded.")
 
   def get_downstream_boundary(self, response):
     content = response.headers.pop('content-type')[0]
@@ -50,12 +54,22 @@ class Avs:
 
     while self.downstram_stop_signal.is_set():
       if len(downstream.data) > 1:
-        new_data, actual_stream.data = read_from_downstream(self.downstream_boundary, actual_stream.data)
+        new_data, downstream.data = read_from_downstream(self.downstream_boundary, downstream.data)
         if len(new_data) > 0:
           print("response:" + new_data)
           # message = parse_data(new_data, self.downstream_boundary)
           # self.process_response_handle(message)
       time.sleep(0.5)
+
+  def read_from_downstream(boundary, data):
+    matching_indices = [n for n, chunk in enumerate(data) if chunk.endswith(boundary)]
+    if not matching_indices:
+        return b'', data
+    boundary_index = matching_indices[-1]
+
+    new_data = data[:boundary_index+1]
+    data = data[boundary_index+1:]
+    return b''.join(new_data), data
 
   def close(self):
     self.downstram_stop_signal.set()
@@ -82,14 +96,21 @@ class Avs:
       return message
 
     first_part_header = self.__message_header_first(boundary_name)
-    first_part_body = self.__message_body_second([], recognize_first_message())
+    first_part_body = self.__message_body_first([], recognize_first_message())
     second_part_header = self.__message_header_second(boundary_name)
-    second_part_body = self.__message_body_second()
+    second_part_body = self.__message_body_second("recording.wav")
 
-    body = first_part_header + '\n\n' first_part_body + '\n\n' + second_part_header + '\n\n' + second_part_body + '\n\n' + self.__end_boundary()
-    r = self.conn.request(method="GET", url=self.__interface("events"), headers=header, body=body)
-    print(r)
-
+    body = first_part_header + '\n' + json.dumps(first_part_body) + '\n' + second_part_header + '\n' + second_part_body + self.__end_boundary(boundary_name)
+    print("[STATE:DEBUG]")
+    print(body)
+    stream_id = self.connection.request(method="GET", url=self.__interface("events"), headers=header, body=body)
+    res = self.connection.get_response(stream_id)
+    if res.status != 200:
+      print(res.read())
+      raise NameError("Bad recognize response %s" % (res.status))
+    print("[STATE:RECOGNIZE] send event.")
+    print(res.read())
+ 
   def close(self, conn):
     conn.close()
 
@@ -128,7 +149,7 @@ class Avs:
     message = ''
     message += self.__begin_boundary(name)
     message += 'Content-Disposition: form-data; name="metadata"\n'
-    message += 'Content-type: application/json; charset=UTF-8\n'
+    message += 'Content-Type: application/json; charset=UTF-8\n'
     return message
 
   def __message_body_first(self, contexts, event):
@@ -140,13 +161,13 @@ class Avs:
   def __message_header_second(self, name):
     message = ''
     message += self.__begin_boundary(name)
-    message += 'Content-Disposition: form-data; name="audio\n'
-    message += 'Content-type: application/octet-stream\n'
+    message += 'Content-Disposition: form-data; name="audio"\n'
+    message += 'Content-Type: application/octet-stream\n'
     return message
 
   def __message_body_second(self, file):
-  	with open('recording.wav') as inf:
-  		data = inf.read()
+    with open(file, "rb") as inf:
+      data = inf.read()
     return data
 
   def __begin_boundary(self, name):
@@ -155,25 +176,5 @@ class Avs:
   def __end_boundary(self, name):
     return '\n--' + name + '--' + '\n'
 
-ContentTypes = {
-  "MULTIPART_FORM_DATA": "multipart/form-data",
-  "JSON": "application/json",
-  "JSON_UTF8": "application/json; charset=UTF-8",
-  "AUDIO": "application/octet-stream"
-}
-
-HttpHeaders = {
-  "CONTENT_TYPE": "Content-Type",
-  "CONTENT_DISPOSITION": "Content-Disposition",
-  "CONTENT_ID": "Content-ID",
-  "AUTHORIZATION": "authorization",
-  "Parameters": {
-    "BOUNDARY": "boundary",
-    "CHARSET": "charset"
-  }
-}
-
 avs = Avs()
-avs.establish()
 avs.recognize()
-avs.close()
