03a5d62cf47ddd2c1bb2e05bea87d46f7577f984
Tomoharu Itoh <tomoharuitoh@TomoharunoMacBook-Pro.local>
2017-01-24 18:37:06 +0900

    refactor

--- device.py
+++ device.py
@@ -91,7 +91,7 @@ class Device:
             if not self.__audio_queue.empty():
                 print("[STATE:DEVICE] alexa response play.")
                 with audio_queue_lock:
-                    audio = self.__audio_queue.get()
+                    audio = self.__audio_queue.get(block=True)
                     play(audio)
 
             if self.__stop_device == True:
