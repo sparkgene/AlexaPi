da9ed1cbac7f9f68850fb95895f7b0d4daec47c4
Tomoharu Itoh <tomoharuitoh@TomoharunoMacBook-Pro.local>
2017-01-25 14:42:24 +0900

    debug

--- device.py
+++ device.py
@@ -66,6 +66,9 @@ class Device:
             t = threading.Timer(5.0, stop_recording)
             t.start()
 
+            while self.__speeching :
+                time.sleep(0.5)
+
             print("[STATE:RECORDING] started 5 seconds")
             while self.__recording == True:
                 l, data = self.__inp.read()
@@ -77,7 +80,7 @@ class Device:
             if self.__avs.active() == False:
                 break
 
-            time.sleep(1.0)
+            time.sleep(0.5)
 
 
     def check_audio_arrival(self):
